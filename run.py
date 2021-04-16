af733c09a36d0b23b3f381e1832f46cd7069b012
SantiagoDGarcia <santiagodaivid@gmail.com>
2021-04-16 11:47:46 -0500

    creacion de listado

--- run.py
+++ run.py
@@ -16,5 +16,14 @@ def suma():
     resultado = 5 + 5
     return "<h3>5 + 5 = %d</h3>" % (resultado)
 
+@app.route('/listado')
+def listado():
+	cadena = ""
+	compras = ["Pollo", "Pavo", "Papa", "Leche", "Pescado"]
+
+	for x in compras:
+		cadena = '%s<li style ="color:red">%s</li>\n' % (cadena,x)
+	return '<h1>Listado de compras</h1> <ul style ="color:red">%s</ul>' % cadena
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8000, debug=True)
